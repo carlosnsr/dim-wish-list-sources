7ed32bc688368338b31f0292ce07b8d1b9e3515b
Carlos Rodrigues <carlos.all.mail@gmail.com>
2020-12-21 23:42:31 -0700

    Prefer proximity_grenades over spike_grenades Added Cold Front, Avalanche. Reworked Deafening Whisper
    Put timed_payload on True Prophecy
    Prefer no-penalty range/stability vs penalised range/stability
    Emphasised liquid_coils in PvP magazines

---
+++ solrac776/src/definitions.js
@@ -0,0 +1,17 @@
+const ITEMS = {
+  821154603: "Gnawing Hunger",
+}
+
+const PERKS = {
+  BARREL: {
+    1467527085: "Full Bore",
+    106909392: "Tactical Mag",
+  },
+  MAGAZINE: {
+    3142289711: "Accurized Rounds",
+  },
+  OTHER: {
+    1890422124: "Tap the Trigger",
+    1015611457: "Kill Clip",
+  }
+}
